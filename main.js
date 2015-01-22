90c3aebe5c91c57e961004d583ea32a6b67fcddf
Dustin Morando <dustinmorando@me.com>
2015-01-22 17:45:32 -0500

    data sheet and html and lincked them

---
+++ main.js
@@ -0,0 +1,26 @@
+(function(){
+  'use strict';
+
+var buffaloBills = rawData.results;
+
+$(document).ready(function(){
+
+  var $list = $('.items-list');
+  buffaloBills.forEach(function(item)
+}
+
+
+  buffaloBills.forEach(function(item){
+    console.log(item.title);
+  });
+  buffaloBills.forEach(function(item){
+    console.log(item.Images);
+  });
+
+
+
+
+
+
+
+})();
