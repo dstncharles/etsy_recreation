500ffb18225947f630aea4da1822149c0a29fa53
Dustin Morando <dustinmorando@me.com>
2015-01-22 18:42:47 -0500

    added code to append into html

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 (function(){
   'use strict';
 
+
+
 var buffaloBills = rawData.results;
 
-$(document).ready(function(){
+  $(document).ready(function(){
 
   var $list = $('.items-list');
-  buffaloBills.forEach(function(item)
-}
-
-
-  buffaloBills.forEach(function(item){
-    console.log(item.title);
-  });
-  buffaloBills.forEach(function(item){
-    console.log(item.Images);
+  buffaloBills.forEach(function(title){
+    var titleText = renderTemplate('title-item', {
+      name: title.name
+      });
+    $list.append(titleText);
   });
 
 
 
 
+  //
+  // buffaloBills.forEach(function(item){
+  //   console.log(item.title);
+  // });
+  // buffaloBills.forEach(function(item){
+  //   console.log(item.Images);
+  // });
 
 
 
-})();
+
+
+
+
+});();
